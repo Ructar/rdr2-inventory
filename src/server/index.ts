b9dea0e6faaa97f7509400f3e1cefdde2f11051d
Ructar <duckhamm@gmail.com>
2021-05-24 19:50:10 +0000

    Inventory server net events

--- src/server/index.ts
+++ src/server/index.ts
@@ -32,6 +32,23 @@ class Container {
             // Anti-Cheat
         }
     }
+
+    lockContainer() {
+        this.locked = true
+    }
+
+    unlockContainer() {
+        this.locked = false
+    }
+
+    addAttribute(attribute: data || {}) {
+        this[attribute.name] = attribute
+    }
+
+    removeAttribute(attribute: string) {
+        delete this[attribute]
+    }
+
 }
 
 on("inventory:createInventory", (type: string, data: {"id": any, "items": any}) => {
@@ -45,3 +62,56 @@ on("inventory:createInventory", (type: string, data: {"id": any, "items": any}) 
     }
 })
 
+on("inventory:requestInventory", (type: string, id: number, cb: function) => {
+    switch(type) {
+        case "Player":
+            cb(PlayerInventories[source][id])
+        case "Horse":
+            cb(HorseInventories[source][id])
+        case "Container":
+            cb(Containers[id])
+    }
+})
+
+on("inventory:destroyInventory", (type: string, id: number) => {
+    switch(type) {
+        case "Player":
+            delete PlayerInventories[source]
+        case "Horse":
+            delete HorseInventories[source]
+        case "Container":
+            delete Containers[id]
+    }
+})
+
+on("inventory:depositItem", (type: string, item_id: number, quantity: number, id?: number) => {
+    // if ( Player Inventory Check ) {
+    switch(type) {
+        case "Player":
+            PlayerInventories[source].depositItem(item_id, quantity)
+        case "Horse":
+            HorseInventories[source].depositItem(item_id, quantity)
+        case "Container":
+            Containers[id].depositItem(item_id, quantity)
+    }
+    // } else {
+        // Anti Cheat
+    //}
+})
+
+on("inventory:withdrawItem", (type: string, item_id: number, quantity: number, id?: number) => {
+    // if ( Player Inventory Check ) {
+    //     if ( Withdrawl Quantity Check ) {
+                switch(type) {
+                    case "Player":
+                        PlayerInventories[source].withdrawItem(item_id, quantity)
+                    case "Horse":
+                        HorseInventories[source].withdrawItem(item_id, quantity)
+                    case "Container":
+                        Containers[id].withdrawItem(item_id, quantity)
+                }
+    //     }
+    // } else {
+    //     Anti Cheat
+    // }
+})
